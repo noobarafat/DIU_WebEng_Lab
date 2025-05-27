09f992cc6a7928ea0838a6754677349410ba62f4
noobarafat <kmarafatislam@gmail.com>
2025-05-27 13:55:54 +0600

    let variable used

--- Lab_3/script.js
+++ Lab_3/script.js
@@ -1,3 +1,13 @@
 function printing(){
-    console.log('Button Clicked')
+let a = 5;
+let b = "Hi";
+let c = 3.5;
+let d = "Hello";
+const e = "61_A1"
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
 }
